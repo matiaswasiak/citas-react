84dac211181c491d6f8ca341496e590d968e8277
Matias Wasiak <matiaswasiak@gmail.com>
2022-09-27 22:19:43 -0300

    Qué es JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Hola Mundo</h1>
+
+      <p>Un parrafo</p>
+
+      <img src="algunaimagen.jpg" alt="" />
+
+      <input type="text" />
     </div>
   );
 }
 
 export default App;
+
+/**
+ * REGLAS JSX
+ * Un elemento que tiene apertura y cierre debe de tener las dos etiquetas, si solamente tiene de apertura tienes que colocar una diagonal antes de cerrarlo.
+ * Cada componente debe tener un return, ya que este mismo es lo que se muestra en pantalla.
+ * Solamente puedes retornar 1 elemento en el nivel mas alto
+ */
