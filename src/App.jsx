0f87b3a3736c4c6aed4b413bf37137a4d439ae34
Matias Wasiak <matiaswasiak@gmail.com>
2022-09-27 22:40:25 -0300

    Extensiones para desarrollar en React en VSCode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Formulario from "./components/Formulario";
 import Header from "./components/Header";
+import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
   return (
     <>
       <Header />
+      <Formulario />
+      <ListadoPacientes />
     </>
   );
 }
