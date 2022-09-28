f452a979125d05f2670ec12737c4ea00a301de7f
Matias Wasiak <matiaswasiak@gmail.com>
2022-09-27 22:57:38 -0300

    Instalando Tailwind CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
-function Header() {
-  return <h1>Desde Header</h1>;
-}
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Desde Header</h1>
+    </header>
+  );
+};
 
 export default Header;
