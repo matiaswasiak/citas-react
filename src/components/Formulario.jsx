0f67f893420c8f4df6ecf34bcfd09d4f768602a1
Matias Wasiak <matiaswasiak@gmail.com>
2022-09-27 23:29:29 -0300

    Creando el Componente de Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Formulario = () => {
-  return <div>Formulario</div>;
+  return (
+    <div className="md:w-1/2 lg:w-2/5">
+      <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+
+      <p className="text-lg mt-5 text-center">
+        Añade Pacientes y{" "}
+        <span className="text-indigo-600 font-bold tex">Administralos</span>
+      </p>
+
+      <form></form>
+    </div>
+  );
 };
 
 export default Formulario;
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ListadoPacientes = () => {
   return (
-    <div>
+    <div className="w-1/2 lg:w-3/5">
       <h2>Listado Pacientes</h2>
     </div>
   );
