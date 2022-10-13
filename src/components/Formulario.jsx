3d2099879c22c10c2a697966c62119ac848ccc09
Matias Wasiak <matiaswasiak@gmail.com>
2022-10-13 12:42:14 -0300

    Mostrar un mensaje de error

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,15 +7,20 @@ const Formulario = () => {
   const [fecha, setFecha] = useState("");
   const [sintomas, setSintomas] = useState("");
 
+  const [error, setError] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // Validación del Formulario
     if ([nombre, propietario, email, fecha, sintomas].includes("")) {
       console.log("Hay al menos un campo vacio");
-    } else {
-      console.log("Todos llenos");
+
+      setError(true);
+      return;
     }
+
+    setError(false);
   };
 
   return (
@@ -31,6 +36,11 @@ const Formulario = () => {
         onSubmit={handleSubmit}
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
       >
+        {error && (
+          <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+            <p>Todos los campos son obligatorios</p>
+          </div>
+        )}
         <div className="mb-5">
           <label
             htmlFor="mascota"
